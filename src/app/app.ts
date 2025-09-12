0c121467987ab3efb534a3e39892375f0b67b25e
kentq6 <kentq6@gmail.com>
2025-09-11 23:33:43 -0400

    Implement and style Header and Home components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Header],
   template: `
-    <h1>Welcome to {{ title() }}!</h1>
-    <p>Hello world</p>
-    
-
-    <router-outlet />
+    <app-header />
+    <main>
+      <app-home />
+    </main>
   `,
   styles: [`
-    p {
-      background-color: red;
+    main {
+      padding: 16px;
     }
   `],
 })
