79ac6a16b9c3413832539a2ea087c2547f2c90ea
kentq6 <kentq6@gmail.com>
2025-09-13 19:54:12 -0400

    Implement event listeners

--- first-ng-app/src/app/home/home.ts
+++ first-ng-app/src/app/home/home.ts
@@ -9,4 +9,8 @@ import { Greeting } from '../components/greeting/greeting';
 })
 export class Home {
   homeMessage = signal('Hello, world! Good day!')
+
+  keyUpHandler(event: KeyboardEvent) {
+    console.log(`user pressed the ${event.key} key`);
+  }
 }
