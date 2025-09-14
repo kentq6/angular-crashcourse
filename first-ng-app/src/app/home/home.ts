d9e7c0ff71f8561617027161322d42b618d83b59
kentq6 <kentq6@gmail.com>
2025-09-13 21:30:20 -0400

    Create Counter component

--- first-ng-app/src/app/home/home.ts
+++ first-ng-app/src/app/home/home.ts
@@ -1,14 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { Greeting } from '../components/greeting/greeting';
+import { Counter } from '../components/counter/counter';
 
 @Component({
   selector: 'app-home',
-  imports: [Greeting],
+  imports: [Greeting, Counter],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
-  homeMessage = signal('Hello, world! Good day!')
+  homeMessage = signal('Hello, world!')
 
   keyUpHandler(event: KeyboardEvent) {
     console.log(`user pressed the ${event.key} key`);
