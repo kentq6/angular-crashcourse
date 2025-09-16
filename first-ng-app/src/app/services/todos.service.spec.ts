dd23693b4e9931338aa96183a814fdc2d4a39a96
kentq6 <kentq6@gmail.com>
2025-09-15 22:21:09 -0400

    Create todo service for hold todo data

---
+++ first-ng-app/src/app/services/todos.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { TodosService } from './todos.service';
+
+describe('Todos', () => {
+  let service: TodosService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(TodosService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
